c10fafac4d714e11c2683ffd3629c6ea24a802dd
yossuli <s1f102302348@iniad.org>
2024-04-29 09:08:36 +0900

    chore: add eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -37,9 +37,9 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-non-null-assertion': 'error',
     '@typescript-eslint/strict-boolean-expressions': 'error',
-    // complexity: ['error', 5],
-    // 'max-depth': ['error', 2],
-    // 'max-nested-callbacks': ['error', 3],
-    // 'max-lines': ['error', 200],
+    complexity: ['error', 5],
+    'max-depth': ['error', 2],
+    'max-nested-callbacks': ['error', 3],
+    'max-lines': ['error', 200],
   },
 };
