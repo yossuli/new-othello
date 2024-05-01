6b0e46674fc10c6fde408100b4a3df8f2a2456b0
yossuli <s1f102302348@iniad.org>
2024-05-01 19:56:46 +0900

    fix \: returned type

--- src/utils/sliceOriginToTurnColor.ts
+++ src/utils/sliceOriginToTurnColor.ts
@@ -2,12 +2,12 @@ import type { LineWithVector } from '../types';
 import { num0or_1 } from './num0or_1';
 import { overflow } from './overflow';
 
-export const sliceOriginToTurnColor = (dir: LineWithVector, turnColor: number) => {
+export const sliceOriginToTurnColor = (dir: LineWithVector, turnColor: number): LineWithVector => {
   const n0or_1 = num0or_1(dir);
   const line = dir.line;
   return {
     ...dir,
-    arr: line.slice(
+    line: line.slice(
       Math.max(
         n0or_1 + 1,
         Math.min(overflow(line.length, line.lastIndexOf(turnColor)), overflow(line.length, n0or_1)),
